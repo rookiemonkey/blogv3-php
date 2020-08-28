b1a0dde860006938ab4b789772a3613efef6452c
rookiemonkey <kevinroirigorbasina@pm.me>
2020-08-28 10:35:35 +0800

    modularized index.php

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website 2014</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
