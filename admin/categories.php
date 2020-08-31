11a4ee42445de1c81a87fb93dc1d846b106061f9
rookiemonkey <kevinroirigorbasina@pm.me>
2020-08-31 09:54:42 +0800

    add category form works with alerts

--- admin/categories.php
+++ admin/categories.php
@@ -18,7 +18,53 @@
                         </h1>
 
                         <div class='col-xs-6'>
-                            <form action=''>
+
+        <?php
+        
+            // check if the form is submitted
+            if(isset($_POST['submit'])) {
+                $category_title = $_POST['cat_title'];
+
+                // validation
+                if(empty($category_title)) { 
+                    echo "<div class='panel panel-danger'>";
+                    echo "<div class='panel-heading'>";
+                    echo "<h3 class='panel-title'>Category field is requied</h3>";
+                    echo "</div>";
+                    echo "</div>";
+                }
+
+                else {
+                    
+                    // prepare statement and query
+                    $category_title = mysqli_real_escape_string($mysqli, $category_title);
+                    $statement = "INSERT INTO categories (cat_title) VALUES (?)";
+                    $query = $mysqli->prepare($statement);
+                    $query->bind_param("s", $category_title);
+                    $result = $query->execute();
+
+                    // check if query is successfull
+                    if($result) { 
+                        echo "<div class='panel panel-success'>";
+                        echo "<div class='panel-heading'>";
+                        echo "<h3 class='panel-title'>Succesfully added a category</h3>";
+                        echo "</div>";
+                        echo "</div>";
+                    }
+                    else { 
+                        echo "<div class='panel panel-danger'>";
+                        echo "<div class='panel-heading'>";
+                        echo "<h3 class='panel-title'>Something went wrong. Please try again later</h3>";
+                        echo "</div>";
+                        echo "</div>";
+                        die(); 
+                    }
+                }
+            }
+
+        ?>
+
+                            <form action='' method='POST'>
                                 <div class="form-group">
                                     <label for="cat_title">Category</label>
                                     <input type="text" class="form-control"  name="cat_title" id="cat_title"/>
