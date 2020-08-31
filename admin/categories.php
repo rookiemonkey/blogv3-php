1755cf30cee29fd1b445080dcdc416b614d54a38
rookiemonkey <kevinroirigorbasina@pm.me>
2020-08-31 16:04:26 +0800

    refactored update category

--- admin/categories.php
+++ admin/categories.php
@@ -114,54 +114,20 @@
                                 </div>
                             </form>
 
-                            <form action='' method='POST'>
-                                <div class="form-group">
-                                    <label for="cat_title">Update Category</label>
-        <?php
-            // query the database - get a category upon click of the update button
-            if(isset($_GET['update'])) {
-                $category_id = $_GET['update'];
-                $category_id = mysqli_real_escape_string($mysqli, $category_id);
-                $statement = "SELECT * FROM categories WHERE cat_id = ?";
-                $query = $mysqli->prepare($statement);
-                $query->bind_param("s", $category_id);
-                $query->execute();
-                $categories = $query->get_result();
+                            <?php
+                                if(isset($_GET['update']) && $_GET['update'] === '') {
+                                    echo "<div class='panel panel-danger'>";
+                                    echo "<div class='panel-heading'>";
+                                    echo "<h3 class='panel-title'>Category id is requied</h3>";
+                                    echo "</div>";
+                                    echo "</div>";
+                                }
 
-                while($row = $categories->fetch_assoc()) {  
-                    $category_id =  $row["cat_id"];
-                    $category_title = $row["cat_title"];
+                                else if (isset($_GET['update'])) { 
+                                    include './includes/update_category.php'; 
+                                }
+                            ?>
 
-        ?>
-                <input 
-                    type="text" 
-                    class="form-control" 
-                    name="cat_title" 
-                    id="cat_title" 
-                    value="<?php if(isset($category_title)) { echo $category_title; } ?>"
-                />
-        <?php
-                }
-            }
-        ?>
-
-        <?php
-            // display an empty input field on initial load
-            if(!isset($_GET['update'])) {
-                echo '<input 
-                    type="text" 
-                    class="form-control" 
-                    name="cat_title" 
-                    id="cat_title" 
-                />';
-            }
-        ?>
-
-                                </div>
-                                <div class="form-group">
-                                    <input type="submit" class="btn btn-primary" name="update_category" value="Update"/>
-                                </div>
-                            </form>
                         </div>
 
         <?php
