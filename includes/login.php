067e449cd86feb081a6a2e72b5d8651a2ce4ef35
rookiemonkey <kevinroirigorbasina@pm.me>
2020-09-01 18:25:53 +0800

    added login w/o session yet

---
+++ includes/login.php
@@ -0,0 +1,39 @@
+<?php
+
+    include 'database.php';
+
+    if(isset($_POST['login'])) {
+        $username = $_POST["username"];
+        $password = $_POST['password'];
+
+        $query = $mysqli->prepare("SELECT * FROM users WHERE user_username = ?");
+        $query->bind_param('s', $username);
+        $query->execute();
+        $users = $query->get_result();
+        
+        if($users->num_rows === 0) {
+            echo "Invalid username and password";
+            die();
+        }
+
+        $row = $users->fetch_assoc();
+        $db_user_id = $row['user_id'];
+        $db_user_password = $row['user_password'];
+        $db_user_username = $row['user_username'];
+        $db_user_firstname = $row['user_firstname'];
+        $db_user_lastname = $row['user_lastname'];
+        $db_user_role = $row['user_role'];
+
+        if($password !== $db_user_password) {
+            header('Location: ../index.php');
+        }
+
+        else {
+            header('Location: ../admin');
+        }
+
+        $query->close();
+    }
+
+
+?>
--- includes/sidebar.php
+++ includes/sidebar.php
@@ -15,6 +15,23 @@
         </form>
     </div>
 
+        <!-- Blog Login Well -->
+    <div class="well">
+        <h4>Login</h4>
+        <form action="./includes/login.php" method="POST">
+            <div class="form-group">
+                <input name="username" type="text" class="form-control" placeholder="Username">
+            </div>
+
+            <div class="input-group">
+                <input name="password" type="password" class="form-control" placeholder="Password">
+                <span class="input-group-btn">
+                    <button class="btn btn-primary" name="login" type="submit">Login</button>
+                </span>
+            </div>
+        </form>
+    </div>
+
     <!-- Blog Categories Well -->
     <div class="well">
 
