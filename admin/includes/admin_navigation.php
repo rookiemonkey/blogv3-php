196402d4345588b7b5d6c691ea449d6cb2e7f391
rookiemonkey <kevinroirigorbasina@pm.me>
2020-09-02 09:04:09 +0800

    render a dashboard chart for admins

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -36,7 +36,7 @@
     <div class="collapse navbar-collapse navbar-ex1-collapse">
         <ul class="nav navbar-nav side-nav">
             <li>
-                <a href="../index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
             </li>
             <li>
                 <a href="profile.php"><i class="fa fa-fw fa-dashboard"></i> Profile</a>
