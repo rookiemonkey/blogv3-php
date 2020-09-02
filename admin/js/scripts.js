00fc05f1fa1ef5cb57379ddee043667d29ffa770
rookiemonkey <kevinroirigorbasina@pm.me>
2020-09-02 14:33:32 +0800

    added select all post checkbox

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,11 +1,27 @@
 $(document).ready(function () {
 
     // text editor
-    ClassicEditor
-        .create(document.querySelector('#body'))
-        .catch(error => {
-            console.error(error);
-        });
+    if (document.querySelector('#body')) {
+        ClassicEditor
+            .create(document.querySelector('#body'))
+            .catch(error => {
+                console.error(error);
+            });
+    }
 
+    // select all posts checkboxes
+    $('#selectAllBoxes').on('click', function () {
+        if (this.checked) {
+            $('.checkBoxes').each(function () {
+                this.checked = true;
+            })
+        }
+
+        else {
+            $('.checkBoxes').each(function () {
+                this.checked = false;
+            })
+        }
+    })
 
 })
