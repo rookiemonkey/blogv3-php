1771f1daf1c3e66bba42331a2edea2d257fe564f
rookiemonkey <kevinroirigorbasina@pm.me>
2020-09-02 09:13:05 +0800

    tweak markup on individual post page

--- post.php
+++ post.php
@@ -57,12 +57,6 @@
 
                             <p><?php echo $post_content ?></p>
 
-                            <a class="btn btn-primary" href="#">
-                                Read More 
-                                <span class="glyphicon glyphicon-chevron-right">
-                                </span>
-                            </a>
-
                             <hr>
 
                 <?php                          
