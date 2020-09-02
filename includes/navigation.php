1515be7af3b1cbca5b2a12fa97630d9245510f99
rookiemonkey <kevinroirigorbasina@pm.me>
2020-09-02 10:21:34 +0800

    tweaked markup, added edit post on individual post page

--- includes/navigation.php
+++ includes/navigation.php
@@ -29,6 +29,17 @@
                 ?>
 
                 <li><a href='/_PHP_blog/admin'>Admin</a></li>
+
+                <?php
+                    if(isset($_SESSION['role'])) {
+
+                        if(isset($_GET['p_id'])) {
+                            $post_id = $_GET['p_id'];
+
+                            echo "<li><a href='/_PHP_blog/admin/posts.php?source=edit_post&p_id={$post_id}'>Edit Post</a></li>";
+                        }
+                    }
+                ?>
                 
             </ul>
         </div>
