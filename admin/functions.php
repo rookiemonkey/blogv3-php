004ee72b166227b66034b6fdb36092f9600af96c
rookiemonkey <kevinroirigorbasina@pm.me>
2020-09-03 10:48:56 +0800

    improved feature: used password_hash and removed the password on edit_user page

--- admin/functions.php
+++ admin/functions.php
@@ -494,13 +494,12 @@
             $user_username = $_POST['user_username'];
             $user_role = $_POST['user_role'];
             $user_email = $_POST['user_email'];
-            $user_password = $_POST['user_password'];
             $user_avatar = "test+image+page";
             $user_randSalt = "test+random+salt";
             
             // prepare statement and query
-            $query = $mysqli->prepare("UPDATE users SET user_firstname = ?, user_lastname = ?, user_username = ?, user_role = ?, user_email = ?, user_password = ?, user_avatar = ?, user_randSalt = ? WHERE user_id = ?");
-            $query->bind_param('sssssssss', $user_firstname, $user_lastname, $user_username, $user_role, $user_email, $user_password, $user_avatar, $user_randSalt, $user_id);
+            $query = $mysqli->prepare("UPDATE users SET user_firstname = ?, user_lastname = ?, user_username = ?, user_role = ?, user_email = ?, user_avatar = ?, user_randSalt = ? WHERE user_id = ?");
+            $query->bind_param('ssssssss', $user_firstname, $user_lastname, $user_username, $user_role, $user_email, $user_avatar, $user_randSalt, $user_id);
             $result = $query->execute();
 
             // check if query is successfull
