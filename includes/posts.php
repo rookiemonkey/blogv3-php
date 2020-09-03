280fad2d99e169d025b3c852f8117d5007232c69
rookiemonkey <kevinroirigorbasina@pm.me>
2020-09-03 08:54:04 +0800

    added feature: pagination

--- includes/posts.php
+++ includes/posts.php
@@ -1,8 +1,39 @@
 <?php
 
+    // initialize variables to be calculated
+    $page;
+    $_page1;
+    $post_per_page = 5;
+
+    if(isset($_GET['page'])) { 
+        $page = $_GET['page']; 
+    } else { 
+        $page = ''; 
+    }
+
+    if($page === '' || $page === 1) { 
+        $page_1 = 0; 
+    } else { 
+        $page_1 = ($page * $post_per_page) - $post_per_page; 
+    }
+
+    // determine the number of posts rows that is published
     $post_status = 'published';
     $query = $mysqli->prepare("SELECT * FROM posts WHERE post_status = ?");
     $query->bind_param('s', $post_status);
+    $query->execute();
+    $results = $query->get_result();
+    $query->close();
+    $total_num = $results->num_rows;
+
+    // determine the number of pages to show and round it down incase it returns
+    // a float instead of an integer
+    $page_last = ceil($total_num / $post_per_page);
+
+
+    $post_status = 'published';
+    $query = $mysqli->prepare("SELECT * FROM posts WHERE post_status = ? LIMIT ?, ?");
+    $query->bind_param('sii', $post_status, $page_1, $post_per_page);
     $query->execute();
     $posts = $query->get_result();
 
