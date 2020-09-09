25b97169a7a58cb9b01bab768f3a93dee5b284b4
rookiemonkey <kevinroirigorbasina@pm.me>
2020-09-09 17:14:47 +0800

    polishing subscriber access

--- controllers/add_comment.php
+++ controllers/add_comment.php
@@ -1,17 +1,29 @@
 <?php
 
-    function add_comment() {
-        if($_POST['comment_content'] !== '') { 
-            $mysqli = Model::Provide_Database();
+function add_comment()
+{
+    if ($_POST['comment_content'] !== '') {
+        $mysqli = Model::Provide_Database();
 
-            $comment_post = intval($_GET['p_id']);
-            $comment_author = $_SESSION['username'];
-            $comment_content = Utility::sanitize($_POST['comment_content']);
-            $comment_date = date("Y-m-d");
-            $comment_status = "unapproved";
+        $comment_post = intval($_GET['p_id']);
+        $comment_post_status = 'published';
+        $comment_author = $_SESSION['username'];
+        $comment_content = Utility::sanitize($_POST['comment_content']);
+        $comment_date = date("Y-m-d");
+        $comment_status = "unapproved";
 
+        // check if the post is published before adding the comment
+        $statement = "SELECT post_id FROM posts WHERE post_id = ? AND post_status = ?";
+        $query = $mysqli->prepare($statement);
+        $query->bind_param("ii", $comment_post, $comment_post_status);
+        $query->execute();
+        $post = $query->get_result();
+
+        if ($post->num_rows === 0) {
+            View::alert_Failed("Something went wrong. Unable to add a comment for an unpublished post");
+        } else {
             $statement = "INSERT INTO comments (comment_post, comment_author, comment_content, comment_status, comment_date) VALUES (?,?,?,?,?)";
-            
+
             $query = $mysqli->prepare($statement);
 
             $query->bind_param("issss", $comment_post, $comment_author, $comment_content, $comment_status, $comment_date);
@@ -21,7 +33,7 @@
             $query->close();
 
             // check if query is successfull
-            if($result) { 
+            if ($result) {
                 View::alert_Success("Succesfully added a comment. Once approved by admin, it will be visible on this post.");
 
                 // increment the comment count on post
@@ -30,14 +42,11 @@
                 $query->bind_param("s", $comment_post);
                 $result = $query->execute();
                 $query->close();
-            }
-            else { 
+            } else {
                 View::alert_Failed("Something went wrong. Please try again later");
             }
         }
-
-        else {
-            View::alert_Failed("Please complete your comment details");
-        }
+    } else {
+        View::alert_Failed("Please complete your comment details");
     }
-?>
+}
--- controllers/add_like.php
+++ controllers/add_like.php
@@ -1,20 +1,32 @@
 <?php
 
-    function add_like() {
-        $mysqli = Model::Provide_Database();
-        
-        $post_id = intval($_GET['p_id']);
-        $user_id = intval($_POST['likedby']);
+function add_like()
+{
+    $mysqli = Model::Provide_Database();
+
+    $post_id = intval($_GET['p_id']);
+    $user_id = intval($_POST['likedby']);
+
+    // check if the post is published before adding the comment
+    $statement = "SELECT post_id FROM posts WHERE post_id = ? AND post_status = ?";
+    $query = $mysqli->prepare($statement);
+    $query->bind_param("ii", $comment_post, $comment_post_status);
+    $query->execute();
+    $post = $query->get_result();
+
+    if ($post->num_rows === 0) {
+        View::alert_Failed("Something went wrong. Unable to add a lik for an unpublished post");
+    } else {
 
         // check if the user already liked the post
-        $isLiked = $mysqli->prepare("SELECT * FROM likes WHERE like_postid = ? AND like_userid = ?");
+        $isLiked = $mysqli->prepare("SELECT post_id FROM likes WHERE like_postid = ? AND like_userid = ?");
         $isLiked->bind_param('ii', $post_id, $user_id);
         $isLiked->execute();
         $results = $isLiked->get_result();
         $isLiked->close();
 
         // not yet liked
-        if($results->num_rows === 0) {
+        if ($results->num_rows === 0) {
             // increment the post_likes col
             $add_like = $mysqli->prepare("UPDATE posts SET post_likes = post_likes + 1 WHERE post_id = ?");
             $add_like->bind_param('i', $post_id);
@@ -43,5 +55,4 @@
             $save_like->close();
         }
     }
-
-?>
+}
