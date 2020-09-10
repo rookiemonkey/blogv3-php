d7a6efd3cf4434ff9862e1cc4b7801786d6428a9
rookiemonkey <kevinroirigorbasina@pm.me>
2020-09-10 16:24:48 +0800

    polished image upload upon registration

--- views/comments.php
+++ views/comments.php
@@ -1,24 +1,38 @@
 <?php
 
-    function render_comments() {
-        $mysqli = Model::Provide_Database();
+function render_comments()
+{
+    $mysqli = Model::Provide_Database();
 
-        $p_id = $_GET['p_id'];
-        $comment_status = 'approved';
-        $query = $mysqli->prepare("SELECT * FROM comments WHERE comment_post = ? AND comment_status = ? ORDER BY comment_id DESC");
-        $query->bind_param('is', $p_id, $comment_status);
+    $p_id = $_GET['p_id'];
+    $comment_status = 'approved';
+    $query = $mysqli->prepare("SELECT * FROM comments WHERE comment_post = ? AND comment_status = ? ORDER BY comment_id DESC");
+    $query->bind_param('is', $p_id, $comment_status);
+    $query->execute();
+    $comments = $query->get_result();
+    $query->close();
+
+    while ($row = $comments->fetch_assoc()) {
+        $comment_author = Utility::sanitize($row['comment_author']);
+        $comment_content = Utility::sanitize($row['comment_content']);
+        $comment_date = Utility::sanitize($row['comment_date']);
+
+        $query = $mysqli->prepare("SELECT user_avatar FROM users WHERE user_username = ?");
+        $query->bind_param('s', $comment_author);
         $query->execute();
-        $comments = $query->get_result();
-        $query->close();
-
-        while($row = $comments->fetch_assoc()) {
-            $comment_author = Utility::sanitize($row['comment_author']);
-            $comment_content = Utility::sanitize($row['comment_content']);
-            $comment_date = Utility::sanitize($row['comment_date']);
+        $data = $query->get_result();
+        $users = $data->fetch_assoc();
 ?>
         <div class="media">
-            <a class="pull-left" href="#">
-                <img class="media-object" src="http://placehold.it/64x64" alt="">
+            <a class="pull-left">
+                <?php
+                if ($users['user_avatar'] === 'https://res.cloudinary.com/promises/image/upload/v1596613153/global_default_image.png') {
+                ?>
+                    <img class="media-object" src="https://res.cloudinary.com/promises/image/upload/v1596613153/global_default_image.png" style="width: 64px; height:64px; border-radius: 20px" />
+                <?php } else { ?>
+                    <img src="/cms/assets/images/avatars/<?php echo Utility::sanitize($users['user_avatar']) ?>" style="width: 64px; height:64px; border-radius: 20px" />
+                <?php } ?>
+
             </a>
             <div class="media-body">
                 <h4 class="media-heading"><?php echo $comment_author; ?>
@@ -28,6 +42,6 @@
             </div>
         </div>
 <?php
-        }
     }
+}
 ?>
