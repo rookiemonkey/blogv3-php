90fc111ce683fe1f0a59b621678236ae84939cf2
rookiemonkey <kevinroirigorbasina@pm.me>
2020-09-10 16:46:58 +0800

    polished image upload upon registration

--- admin/views/admin_navigation.php
+++ admin/views/admin_navigation.php
@@ -22,15 +22,14 @@
         </li>
         <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                <i class="fa fa-user"></i>
-                <?php echo Utility::sanitize($_SESSION['firstname']); ?>
+                <?php View::Avatar($_SESSION['avatar'], '24px') ?>
                 <b class="caret"></b>
             </a>
             <ul class="dropdown-menu">
                 <li>
                     <a href="#">
                         <i class="fa fa-fw fa-user"></i>
-                        Profile
+                        <?php echo Utility::sanitize($_SESSION['firstname']); ?>
                     </a>
                 </li>
                 <li class="divider"></li>
